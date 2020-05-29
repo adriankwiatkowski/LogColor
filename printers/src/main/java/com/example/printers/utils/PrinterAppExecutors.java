00554e5582876ffa2403f72f0aeda9b3959586c7
adriankwiatkowski <adi78900@gmail.com>
2020-05-29 23:06:07 +0200

    Add shutdown method to printer library.

--- printers/src/main/java/com/example/printers/utils/PrinterAppExecutors.java
+++ printers/src/main/java/com/example/printers/utils/PrinterAppExecutors.java
@@ -1,15 +1,19 @@
 package com.example.printers.utils;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PrinterAppExecutors {
 
+    private static final int TIMEOUT_TERMINATION_SECONDS = 60;
+
     private static final Object LOCK = new Object();
     private static PrinterAppExecutors sInstance;
-    private final Executor mLogExecutor;
+    private final ExecutorService mLogExecutor;
 
-    private PrinterAppExecutors(Executor logExecutor) {
+    private PrinterAppExecutors(ExecutorService logExecutor) {
         this.mLogExecutor = logExecutor;
     }
 
@@ -28,5 +32,28 @@ public class PrinterAppExecutors {
 
     public Executor logThread() {
         return mLogExecutor;
+    }
+
+    public void shutdownExecutors() {
+        shutdownAndAwaitTermination(mLogExecutor);
+    }
+
+    private void shutdownAndAwaitTermination(ExecutorService executorService) {
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(
+                    TIMEOUT_TERMINATION_SECONDS, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+                if (!executorService.awaitTermination(
+                        TIMEOUT_TERMINATION_SECONDS, TimeUnit.SECONDS)) {
+                    // Error terminating thread.
+                    System.err.println("ExecutorService did not terminate.");
+                }
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
     }
 }
